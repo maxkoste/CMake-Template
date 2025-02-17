ac8dbc99404348ac27ec59ec1dd32c0b2b36cf56
Max Koste <maxkoste@gmail.com>
2025-02-17 17:06:46 +0100

    initial commit, project structure for C++ projects with CMake

---
+++ CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/maxkoste/IdeaProjects/C_projects/CMakeTemplate/bin/MyProject"
+  "/Users/maxkoste/IdeaProjects/C_projects/CMakeTemplate/bin/MyProject.pdb"
+  "CMakeFiles/MyProject.dir/src/main.cpp.o"
+  "CMakeFiles/MyProject.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
